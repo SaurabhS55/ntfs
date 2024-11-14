e269285e4b56cfef1b1634bd9fffb8c5f8fb9a19
Saurabh <saurabhsalunke91@gmail.com>
2024-11-14 08:13:02 +0530

    kmeans added

--- kmeans/kmeans.cpp
+++ kmeans/kmeans.cpp
@@ -1,107 +1,68 @@
-#include <bits/stdc++.h>
-
+#include<bits/stdc++.h>
 using namespace std;
 
-struct Point {
-    double x, y;
-};
-
-vector<Point> initializeCentroids(const vector<Point>& data, int k) {
-    vector<Point> centroids;
-    srand(time(0));
-    for (int i = 0; i < k; ++i) {
-        centroids.push_back(data[rand() % data.size()]);
-    }
-    return centroids;
-}
-
-vector<vector<Point>> assignClusters(const vector<Point>& data, const vector<Point>& centroids) {
-    vector<vector<Point>> clusters(centroids.size());
-    for (const auto& point : data) {
-        double minDist = numeric_limits<double>::max();
-        int minIndex = 0;
-
-        for (int i = 0; i < centroids.size(); ++i) {
-            double dist = sqrt(pow(point.x - centroids[i].x, 2) + pow(point.y - centroids[i].y, 2));
-            if (dist < minDist) {
-                minDist = dist;
-                minIndex = i;
+vector<vector<pair<double, double>>> kmeans(const vector<double>& x, const vector<double>& y, int k) {
+    int n = x.size();
+    vector<pair<double, double>> centroids(k);
+    for(int i = 0; i < k; i++) centroids[i] = {x[i], y[i]};
+    
+    vector<vector<pair<double, double>>> clusters(k);
+    for(int it = 0; it < 10; it++) {
+        for(auto& c : clusters) c.clear();
+        
+        for(int i = 0; i < n; i++) {
+            int best = 0;
+            double best_dist = hypot(x[i] - centroids[0].first, y[i] - centroids[0].second);
+            for(int j = 1; j < k; j++) {
+                double d = hypot(x[i] - centroids[j].first, y[i] - centroids[j].second);
+                if(d < best_dist) {
+                    best_dist = d;
+                    best = j;
+                }
+            }
+            clusters[best].push_back({x[i], y[i]});
+        }
+        
+        for(int j = 0; j < k; j++) {
+            if(!clusters[j].empty()) {
+                double sum_x = 0, sum_y = 0;
+                for(auto& p : clusters[j]) {
+                    sum_x += p.first;
+                    sum_y += p.second;
+                }
+                centroids[j] = {sum_x / clusters[j].size(), sum_y / clusters[j].size()};
             }
         }
-        clusters[minIndex].push_back(point);
     }
-    return clusters;
+    cout << "\ncentroids:\n";
+    for (int i = 0; i < k;i++){
+        cout << centroids[i].first << " " << centroids[i].second << endl;
+    }
+        return clusters;
 }
 
-vector<Point> updateCentroids(const vector<vector<Point>>& clusters) {
-    vector<Point> newCentroids;
-    for (const auto& cluster : clusters) {
-        if (cluster.empty()) continue;
-
-        double avgX = 0.0, avgY = 0.0;
-        for (const auto& point : cluster) {
-            avgX += point.x;
-            avgY += point.y;
-        }
-        avgX /= cluster.size();
-        avgY /= cluster.size();
-        newCentroids.push_back({avgX, avgY});
-    }
-    return newCentroids;
-}
-
-vector<vector<Point>> kMeans(vector<Point>& data, int k,) {
-    vector<Point> centroids = initializeCentroids(data, k);
-    ofstream out("output.csv");
-
-    for (int iter = 0; iter < maxIters; ++iter) {
-        out << "\nIteration " << iter + 1 << ":\n";
-        vector<vector<Point>> clusters = assignClusters(data, centroids);
-
-        for (int i = 0; i < clusters.size(); ++i) {
-            out << "Cluster " << i + 1 << " - Centroid: (" << centroids[i].x << ", " << centroids[i].y << ")\n";
-            out << "Points: ";
-            for (const auto& point : clusters[i]) {
-                out << "(" << point.x << ", " << point.y << ") ";
-            }
-            out << "\n";
-        }
-
-        vector<Point> newCentroids = updateCentroids(clusters);
-        if (newCentroids == centroids) break;
-
-        centroids = newCentroids;
-    }
-
-    out << "\nFinal Clusters and Points:\n";
-    for (int i = 0; i < centroids.size(); ++i) {
-        out << "Cluster " << i + 1 << " - Centroid: (" << centroids[i].x << ", " << centroids[i].y << ")\n";
-        out << "Points: ";
-        for (const auto& point : clusters[i]) {
-            out << "(" << point.x << ", " << point.y << ") ";
-        }
-        out << "\n";
-    }
-
-    out.close();
-    return clusters;
-}
 int main() {
-    int k = 3;
-
-    vector<Point> data;
     ifstream in("input.csv");
-    string line;
-    while (getline(in, line)) {
-        stringstream ss(line);
-        string temp;
-        Point point;
-        getline(ss, temp, ',');
-        point.x = stod(temp);
-        getline(ss, temp, ',');
-        point.y = stod(temp);
-        data.push_back(point);
+    vector<double> x, y;
+    string str;
+    while(getline(in, str)) {
+        stringstream ss(str);
+        string tmp;
+        getline(ss, tmp, ',');
+        x.push_back(stod(tmp));
+        getline(ss, tmp, ',');
+        y.push_back(stod(tmp));
     }
-    kMeans(data, k);
-    return 0;
+    int k;
+    cout << "\nEnter number of clusters (k): " << endl;
+    cin >> k;
+    vector<vector<pair<double, double>>> clusters = kmeans(x, y, k);
+    cout << "\nclusters: " << endl;
+    for (int i = 0; i < clusters.size();i++){
+        for (int j = 0; j < clusters[i].size();j++){
+            cout << clusters[i][j].first << " " << clusters[i][j].second<<" ";
+        }
+        cout << endl;
+    }
+        return 0;
 }
